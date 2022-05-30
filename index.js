6d25faccd50edaee694bdbff5ca424f736de4b86
Nick Story <storynickolasl@gmail.com>
2022-05-30 13:40:34 -0400

    commit tree 850ca50c21e2a3aacbd3bf95b8601d00684d9e79

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email
+      })
+    })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object)
+      document.body.append(object.id)
+    })
+    .catch(function (error) {
+      document.body.append(error.message)
+    })
+}
+
+
+  
+
+
+   
+
